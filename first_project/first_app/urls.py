f97620fc17647435171510fab5b52a0a0e2d36b3
ikshit <jalanikshit1@gmail.com>
2019-04-06 18:01:16 +0530

    commit

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from first_app import views
 
+# Template Tagging
+app_name = "first_app"
+
 urlpatterns = [
     url(r'^$',views.index,name = "index"),
+    url(r'^relative/$',views.relative,name = 'relative'),
+    url(r'^other/$',views.other,name = 'other')
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -38,3 +38,9 @@ def users(request):
          else:
             print("Invalid form ERROR!")
     return render(request,'first_app/user.html',{'form':form})
+
+def other(request):
+    return render(request,'first_app/other.html')
+
+def relative(request):
+    return render(request,'first_app/relative_url_templates.html')
